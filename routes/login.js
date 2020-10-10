928a5cf8a8519ed169bd62c50c0dae47c8430396
Alfonso <is713932@iteso.mx>
2020-10-10 17:50:07 -0500

    Feat: Add hash password functionality wih argon2

--- routes/login.js
+++ routes/login.js
@@ -6,74 +6,102 @@ const path = require('path')
 let dialog = require('dialog')
 const speakeasy = require('speakeasy')
 const qrcode = require('qrcode')
+const  argon2 = require('argon2')
 
 
 
 router.route('/envioCredenciales')
-    .post((req,res) => {
-      let {email, password} = req.body;
-    //  console.log("email: ", email, " , password:"+password+"\n"); 
-      
-      //si existe el cliente en la BD, se deja pasar
-      if(credencialesBD.find(c => c.correo == email && c.password == password)){
+  .post(async (req, res) => {
+    let { email, password } = req.body;
 
-        //Generar codigo qrcode para verificacion de doble factor
+
+    //si existe el usuario en la BD, se deja pasar
+    if (credencialesBD.find(c => c.correo == email)) {
+
+
+      try {
+
+        //Obtener el indice del usuario
+        let index = credencialesBD.findIndex(c => c.correo == email);
+  
+        //Verificar contraseña con argon2
+        if(await argon2.verify(credencialesBD[index].password, password)){ // si entra la contraseña es verificada
+
+          //Generar codigo qrcode para verificacion de doble factor
         let date = new Date();
         let secret = speakeasy.generateSecret({
-          name:"ProyectoSeguridad_"+date.getMinutes()+date.getSeconds()
+          name: "ProyectoSeguridad_" + date.getMinutes() + date.getSeconds()
         })
 
-        
+
         //generar qrcode
-         qrcode.toDataURL(secret.otpauth_url, (err,data)=> {
+        qrcode.toDataURL(secret.otpauth_url, (err, data) => {
 
-          if(err) throw err;
+          if (err) throw err;
 
-          else{ // si se genero la url para el codigo qr se avanza, y se pasa dicha url como argumento
-           
+          else { // si se genero la url para el codigo qr se avanza, y se pasa dicha url como argumento
+
             //Guardar el secret ascii para futuras referencias
-           multifactorAuth[0].idAscii = secret.ascii
+            multifactorAuth[0].idAscii = secret.ascii
             let rutaSecret = '../multifactorAuth/secret.json'
             const filePathSecret = path.join(__dirname, rutaSecret);
             fs.writeFileSync(filePathSecret, JSON.stringify(multifactorAuth));
 
 
             //poner la bandera del usuario de "enProceso2fa" en 1 para decir que se va a pasar a la verificacion
-            let index = credencialesBD.findIndex(c => c.correo == email && c.password == password);
+            let index = credencialesBD.findIndex(c => c.correo == email);
             credencialesBD[index].enProceso2fa = 1;
 
             let rutaBD = '../credencialesBD.json'
-           const filePathBD = path.join(__dirname, rutaBD);
+            const filePathBD = path.join(__dirname, rutaBD);
             fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
 
             //renderizar la siguiente pagina de verificacion
-            res.render('verifyGoogleAuth',{
-              title:"Google Authenticator",
+            res.render('verifyGoogleAuth', {
+              title: "Google Authenticator",
               condition: false,
-              qrcode : [{url:data}]
+              qrcode: [{ url: data }]
             });
           }
-          
-         })
+
+        })
+
+
+
+        }else{ //contraseña incorrecta
+          dialog.err("Usuario o contraseña no válidos", "Login incorrecto");
+           res.render('login');
+        }
+
+
+        
+
+
+      } catch (err) {
+        console.log(err)
       }
-      
-      else{
-        dialog.err("Usuario o contraseña no válidos","Login incorrecto");
-        res.render('login');
-      }
-    })
+
+
+
+    }
+
+    else {
+      dialog.err("Usuario o contraseña no válidos", "Login incorrecto");
+      res.render('login');
+    }
+  })
 
 router.route('/logout')
-    .post((req,res)=>{
+  .post((req, res) => {
 
-      //Encontrar usuario activo en la base de datos y pasarlo a inactivo
-      let index = credencialesBD.findIndex(c => c.usuarioActivo == 1 || c.enProceso2fa == 1);
-      credencialesBD[index].usuarioActivo = 0;
-      credencialesBD[index].enProceso2fa = 0;
-      let rutaBD = '../credencialesBD.json'
-      const filePathBD = path.join(__dirname, rutaBD);
-      fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
+    //Encontrar usuario activo en la base de datos y pasarlo a inactivo
+    let index = credencialesBD.findIndex(c => c.usuarioActivo == 1 || c.enProceso2fa == 1);
+    credencialesBD[index].usuarioActivo = 0;
+    credencialesBD[index].enProceso2fa = 0;
+    let rutaBD = '../credencialesBD.json'
+    const filePathBD = path.join(__dirname, rutaBD);
+    fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
 
-      res.render('login')
-    })
+    res.render('login')
+  })
 module.exports = router;
--- routes/registerUser.js
+++ routes/registerUser.js
@@ -3,42 +3,55 @@ let credencialesBD = require('../credencialesBD.json')
 const fs = require('fs')
 const path = require('path')
 const dialog = require('dialog')
-
+const argon2 = require('argon2')
 
 //Llevar a pagina de registro de usuario
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('registroUsuarios')
 });
 
 //Crear nuevo usuario
-router.post('/nuevoUsuario',(req,res)=>{
-    let {correo,password} = req.body
-    
+router.post('/nuevoUsuario', async (req, res) => {
+    let { correo, password } = req.body
+
     //verificar que no exista un usuario dado de alta con el correo
-    if(!(credencialesBD.find(c => c.correo == correo))){ // regresa true si existe el usuario, entonces se niega la condicion para que cuando no ecuentre un usuario se pueda entrar
-        //meter el nuevo usuario a la BD
-        console.log(credencialesBD[0].id)
-        credencialesBD.push({
-            "id": credencialesBD[credencialesBD.length-1].id + 1,
-            "correo": correo,
-            "password": password,
-            "cantidadRegistros": 0,
-            "ultimoRegistro": "",
-            "usuarioActivo": 0,
-            "enProceso2fa": 0
-        })
+    if (!(credencialesBD.find(c => c.correo == correo))) { // regresa true si existe el usuario, entonces se niega la condicion para que cuando no ecuentre un usuario se pueda entrar
 
-        //Actualizar el json de la BD
-        let rutaBD = '../credencialesBD.json'
-        const filePathBD = path.join(__dirname, rutaBD);
-         fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
+        try {
 
-         //mandar al login
-         res.render('login')
-    
+
+            //Usar argon2 para encriptar contraseña
+            const hash = await argon2.hash(password)
+
+            //meter el nuevo usuario a la BD
+            
+            credencialesBD.push({
+                "id": credencialesBD[credencialesBD.length - 1].id + 1,
+                "correo": correo,
+                "password": hash,
+                "cantidadRegistros": 0,
+                "ultimoRegistro": "",
+                "usuarioActivo": 0,
+                "enProceso2fa": 0
+            })
+
+            //Actualizar el json de la BD
+            let rutaBD = '../credencialesBD.json'
+            const filePathBD = path.join(__dirname, rutaBD);
+            fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
+
+            //mandar al login
+            res.render('login')
+        } catch (err) {
+            console.log(err);
+        }
+
+
+
+
     }
-    else{ // si ya existe un usuario registrado con el correo proporcionado
-        dialog.err("Usuario ya existente","Registro invalido");
+    else { // si ya existe un usuario registrado con el correo proporcionado
+        dialog.err("Usuario ya existente", "Registro invalido");
         res.render('login')
     }
 });
