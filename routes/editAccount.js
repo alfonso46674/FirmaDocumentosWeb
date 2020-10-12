1ff0b78925f85762ce5ab5b637d7068846adb469
Alfonso <is713932@iteso.mx>
2020-10-12 15:49:40 -0500

    Fix: Rework edit account functionality.
    When the user edits its password, now it needs to provide the old password as well as the new password.
    The new password is also encrypted with argon2.

--- routes/editAccount.js
+++ routes/editAccount.js
@@ -2,19 +2,35 @@ const router = require('express').Router()
 let credencialesBD = require('../credencialesBD.json')
 const fs = require('fs')
 const path = require('path')
+const argon2 = require('argon2')
+const dialog = require('dialog')
 
-router.post('/changePassword',(req,res)=>{
-    let {password} = req.body
+router.post('/changePassword', async (req, res) => {
+    let { oldPassword, newPassword } = req.body
+
     let index = credencialesBD.findIndex(c => c.usuarioActivo == 1);
-   credencialesBD[index].password = password
-   
-    //hacer un logout manual, poner la bandera de usuario activo en 0 y pasar los cambios a la BD
-   credencialesBD[index].usuarioActivo = 0;
-   let rutaBD = '../credencialesBD.json'
-   const filePathBD = path.join(__dirname, rutaBD);
-   fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
 
-   res.render('login')
+    //verificar que la contraseña antigua sea la correcta
+    if (await argon2.verify(credencialesBD[index].password, oldPassword)) {
+
+        //encriptar nueva contraseña
+        const hash = await argon2.hash(newPassword);
+        credencialesBD[index].password = hash
+        //hacer un logout manual, poner la bandera de usuario activo en 0 y pasar los cambios a la BD
+        credencialesBD[index].usuarioActivo = 0;
+        let rutaBD = '../credencialesBD.json'
+        const filePathBD = path.join(__dirname, rutaBD);
+        fs.writeFileSync(filePathBD, JSON.stringify(credencialesBD));
+
+        res.render('login')
+
+    } else { // contraseña incorrecta
+        dialog.err("Contraseña antigua incorrecta", "Edicion invalida");
+    }
+
+
+
+
 
 })
 
